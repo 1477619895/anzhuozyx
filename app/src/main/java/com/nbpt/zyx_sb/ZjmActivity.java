0792f100d65b9854779481ae8bd495bf4a10d1de
1477619895 <1477619895@qq.com>
2020-11-19 10:25:08 +0800

    1119

---
+++ app/src/main/java/com/nbpt/zyx_sb/ZjmActivity.java
@@ -0,0 +1,13 @@
+package com.nbpt.zyx_sb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ZjmActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zjm);
+    }
+}
