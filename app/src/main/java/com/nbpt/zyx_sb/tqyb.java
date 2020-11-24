91135537813226ff187d5cb2683ea44bac8b683d
1477619895 <1477619895@qq.com>
2020-11-24 14:10:05 +0800

    2020.11.24  14:00

---
+++ app/src/main/java/com/nbpt/zyx_sb/tqyb.java
@@ -0,0 +1,13 @@
+package com.nbpt.zyx_sb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class tqyb extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tqyb);
+    }
+}
