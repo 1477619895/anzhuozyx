c84c0bbe1c6767a82e363420508eb8dc82220a02
1477619895 <1477619895@qq.com>
2020-12-01 14:52:56 +0800

    2020 12 1   14.55

--- app/src/main/java/com/nbpt/zyx_sb/lianxiwomen.java
+++ app/src/main/java/com/nbpt/zyx_sb/lianxiwomen.java
@@ -1,15 +1,32 @@
 package com.nbpt.zyx_sb;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class lianxiwomen extends Activity {
-
+    TextView dh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lianxiwomen);
 
-
+        TextView dhhm = findViewById(R.id.dh);
+        dhhm.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+        TextView wangzhan = findViewById(R.id.WZ);
+        wangzhan.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
+        public void qwe (View view)
+        {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            Uri data =Uri.parse("tel:13505741234");
+            intent.setData(data);
+            startActivity(intent);
+
+        }
+
 }
--- app/src/main/java/com/nbpt/zyx_sb/szzx.java
+++ app/src/main/java/com/nbpt/zyx_sb/szzx.java
@@ -1,13 +1,42 @@
 package com.nbpt.zyx_sb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 public class szzx extends Activity {
-
+    ListView qwe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_szzx);
+        ListView listView = findViewById(R.id.qq);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:{Intent intent = new Intent(szzx.this, zhinengjiaju.class);
+                        startActivity(intent);break;}
+                    case 1:{Intent intent1 = new Intent(szzx.this, znsc.class);
+                        startActivity(intent1);break;}
+                    case 2:{Intent intent2 = new Intent(szzx.this, tqyb.class);
+                        startActivity(intent2);break;}
+                    case 3:{Intent intent3 = new Intent(szzx.this, lianxiwomen.class);
+                        startActivity(intent3);break;}
+                }
+            }
+        });
+
+
+
     }
+
 }
